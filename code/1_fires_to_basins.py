8158fa7b92bff7ffa442ec63945125a69c3bb9a2
Joel Ferguson <49797915+joelferg@users.noreply.github.com>
2021-08-09 17:08:52 -0700

    Pull EPA PM2.5 Data for Sac Valley counties

--- code/1_fires_to_basins.py
+++ code/1_fires_to_basins.py
@@ -4,7 +4,7 @@ import shapely.geometry as shg
 import datetime
 from math import floor
 import ee
-
+'''
 # Read in air basins and re-project
 air_basins = gpd.read_file("../raw_data/maps/california-air-resources-board-air-basin-boundaries/CaAirBasin.shp")
 air_basins = air_basins.to_crs("EPSG:4326")
@@ -26,31 +26,38 @@ basin_fires['year'] = basin_fires['acq_date'].apply(lambda x: x.year)
 
 # Save it
 basin_fires.to_pickle("../processed_data/basin_fires_2012-2020.pkl")
-
-basin_fires_2020 = basin_fires.loc[basin_fires['year']==2020]
+'''
+basin_fires = pd.read_pickle("../processed_data/basin_fires_2012-2020.pkl")
+sac_fires = basin_fires.loc[basin_fires['NAME']=="Sacramento Valley"]
+#basin_fires_2020 = basin_fires.loc[basin_fires['year']==2020]
 
 ee.Initialize()
-cropland = ee.ImageCollection("USDA/NASS/CDL").filterDate("2020-01-01","2021-01-01").first()
-cropland = cropland.select("cultivated")
-for i in range(1,11):
-    print(i)
-    start = (i-1)*floor(len(basin_fires_2020['index'])/10)
-    if i != 10:
-        end = (i)*floor(len(basin_fires_2020['index'])/10)-1
+for year in range(2012,2021):
+    if year != 2012:
+        cropland = ee.ImageCollection("USDA/NASS/CDL").filterDate(f"{year}-01-01",f"{year+1}-01-01").first()
     else:
-        end = len(basin_fires_2020['index'])-1
+        cropland = ee.ImageCollection("USDA/NASS/CDL").filterDate("2013-01-01","2014-01-01").first()
+    cropland = cropland.select("cultivated")
+    sac_fires_year = sac_fires.loc[sac_fires['year']==year]
+    for i in range(1,5):
+        print(i)
+        start = (i-1)*floor(len(sac_fires_year['index'])/4)
+        if i != 4:
+            end = (i)*floor(len(sac_fires_year['index'])/4)-1
+        else:
+            end = len(sac_fires_year['index'])-1
 
-    basin_fires_i = basin_fires_2020.iloc[start:end,:]
-    basin_fires_i['ee_point'] = basin_fires_i.apply(lambda x: ee.Geometry.Point(x['longitude'],x['latitude']),axis=1)
-    basin_fires_i['ee_feature'] = basin_fires_i.apply(lambda x: ee.Feature(x['ee_point'],{'index':x['index'],'year':x['year']}), axis=1)
-    ee_basin_fires = ee.FeatureCollection(basin_fires_i['ee_feature'].tolist())
+        sac_fires_i = sac_fires_year.iloc[start:end,:]
+        sac_fires_i['ee_point'] = sac_fires_i.apply(lambda x: ee.Geometry.Point(x['longitude'],x['latitude']),axis=1)
+        sac_fires_i['ee_feature'] = sac_fires_i.apply(lambda x: ee.Feature(x['ee_point'],{'index':x['index'],'year':x['year']}), axis=1)
+        ee_sac_fires_i = ee.FeatureCollection(sac_fires_i['ee_feature'].tolist())
 
-    ee_basin_fires = ee_basin_fires.map(lambda x: x.buffer(30))
+        ee_sac_fires_i = ee_sac_fires_i.map(lambda x: x.buffer(30))
 
-    out = cropland.reduceRegions(ee_basin_fires,
-                                 ee.Reducer.max(),
-                                 1)
-    task = ee.batch.Export.table.toDrive(collection=out,
-                                         folder='cal_fires',
-                                             description=f"cal_fires_cropland_2020_{i}")
-    task.start()
+        out = cropland.reduceRegions(ee_sac_fires_i,
+                                     ee.Reducer.max(),
+                                     1)
+        task = ee.batch.Export.table.toDrive(collection=out,
+                                             folder='cal_fires',
+                                                 description=f"sac_fires_cropland_{year}_{i}")
+        task.start()
---
+++ code/6_pull_pm25.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import geopandas as gpd
+import datetime
+import requests
+import re
+import ast
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+email = os.getenv("aqi_email")
+key = os.getenv("aqi_key")
+
+def processAQI(url):
+    req = requests.get(url)
+    text = req.text
+    text = re.sub("/s","",text)
+    text = re.sub("null","None",text)
+    text = ast.literal_eval(text)
+    if len(text['Data'])>0:
+        df = pd.DataFrame(text['Data'])
+        return(df)
+    else:
+        print("No data")
+        pass
+
+def addZeros(x,n):
+    x = str(x)
+    while len(x)<n:
+        x = "0"+x
+    return(x)
+
+counties = gpd.read_file("../raw_data/maps/CA_Counties/CA_Counties_TIGER2016.shp")
+counties = counties.to_crs("EPSG:4326")
+
+basins = gpd.read_file("../raw_data/maps/california-air-resources-board-air-basin-boundaries/CaAirBasin.shp")
+basins = basins.to_crs("EPSG:4326")
+sac_valley = basins.loc[basins['NAME']=="Sacramento Valley"]
+
+sac_counties = gpd.sjoin(counties,sac_valley,
+                        how="inner",op="intersects")
+sac_counties = sac_counties['COUNTYFP'].to_list()
+
+
+already_pulled = os.listdir("../raw_data/pm25/")
+for year in range(2012,2021):
+    #end_date = datetime.date(year+(month//12),(month%12)+1,1)-datetime.timedelta(days=1)
+    for county in sac_counties:
+        if f"pm25_06{county}_{year}.pkl" not in already_pulled:
+            url = f"https://aqs.epa.gov/data/api/sampleData/byCounty?email={email}&key={key}&param=88101&bdate={year}0101&edate={year}{12}{31}&state=06&county={county}"
+            df = processAQI(url)
+            if type(df)!=type(None):
+                df.to_pickle(f"../raw_data/pm25/pm25_06{county}_{year}.pkl")
+            time.sleep(6)
